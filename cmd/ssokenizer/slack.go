d7d6e62c534d7a966dc0a9c069a31622a75f6ebc
Matt Brown <matt@mkmba.nz>
2024-07-12 14:12:01 +1200

    Support retrieving custom Slack metadata from token
    Adds a CustomExchange config pointer that allows a provider to replace
    the default oauth2.Exchange routine with a custom implementation that
    returns a metadata string in addition to the normal Token. The metadata
    string is returned back to the caller in the response alongside the
    sealed parameter.
    
    The slack profile uses this in conjunction with the slack-go package to
    return a copy of the additional metadata fields returned by the
    oauth.v2.access API (stripped off all tokens).
    
    Also upgrades the Slack profile to use the /v2 Slack OAuth endpoints.

---
+++ cmd/ssokenizer/slack.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/slack-go/slack"
+	"golang.org/x/oauth2"
+)
+
+// Implements a custom version of oauth.Config.Exchange that handles the additional metadata Slack returns alongside the access/refresh
+// tokens, and returns it as a gzipped, base64 encoded string after stripping any tokens from it.
+func slackExchange(ctx context.Context, conf oauth2.Config, code string) (*oauth2.Token, string, error) {
+	r, err := slack.GetOAuthV2ResponseContext(ctx, http.DefaultClient, conf.ClientID, conf.ClientSecret, code, conf.RedirectURL)
+	if err != nil {
+		return nil, "", err
+	}
+	// Convert response to oauth2.Token
+	tok := oauth2.Token{
+		AccessToken:  r.AccessToken,
+		RefreshToken: r.RefreshToken,
+	}
+	if r.ExpiresIn != 0 {
+		tok.Expiry = time.Now().Add(time.Duration(r.ExpiresIn) * time.Second)
+	}
+	// Strip token(s) from response
+	r.AccessToken = ""
+	r.RefreshToken = ""
+	r.AuthedUser.AccessToken = ""  // TODO: no useful way to return this from ssokenizer currently.
+	r.AuthedUser.RefreshToken = "" // TODO: no useful way to return this from ssokenizer currently.
+	// Serialize stripped token back as metadata
+	mdB, err := json.Marshal(r)
+	if err != nil {
+		return nil, "", err
+	}
+	return &tok, base64.StdEncoding.EncodeToString(mdB), nil
+}
