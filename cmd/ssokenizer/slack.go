0c240cfd9fc8f1789a47a8cafdc320b5a4369628
Matt Brown <matt@mkmba.nz>
2024-07-17 00:51:07 +1200

    add custom slack refresh option

--- cmd/ssokenizer/slack.go
+++ cmd/ssokenizer/slack.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
@@ -19,13 +20,7 @@ func slackExchange(ctx context.Context, conf oauth2.Config, code string) (*oauth
 		return nil, "", err
 	}
 	// Convert response to oauth2.Token
-	tok := oauth2.Token{
-		AccessToken:  r.AccessToken,
-		RefreshToken: r.RefreshToken,
-	}
-	if r.ExpiresIn != 0 {
-		tok.Expiry = time.Now().Add(time.Duration(r.ExpiresIn) * time.Second)
-	}
+	tok := slackToToken(r)
 	// Strip token(s) from response
 	r.AccessToken = ""
 	r.RefreshToken = ""
@@ -36,5 +31,27 @@ func slackExchange(ctx context.Context, conf oauth2.Config, code string) (*oauth
 	if err != nil {
 		return nil, "", err
 	}
-	return &tok, base64.StdEncoding.EncodeToString(mdB), nil
+	return tok, base64.StdEncoding.EncodeToString(mdB), nil
+}
+
+// Implements a custom version of oauth.Refresh that deals with the fact slack returns a token type other than Bearer which
+// the oauth2 package does not like - metadata is not actually used here!
+func slackRefresh(ctx context.Context, conf oauth2.Config, refreshToken *oauth2.Token) (*oauth2.Token, error) {
+	fmt.Println("Refresh with token: ", refreshToken.RefreshToken)
+	r, err := slack.RefreshOAuthV2TokenContext(ctx, http.DefaultClient, conf.ClientID, conf.ClientSecret, refreshToken.RefreshToken)
+	if err != nil {
+		return nil, err
+	}
+	return slackToToken(r), nil
+}
+
+func slackToToken(r *slack.OAuthV2Response) *oauth2.Token {
+	tok := oauth2.Token{
+		AccessToken:  r.AccessToken,
+		RefreshToken: r.RefreshToken,
+	}
+	if r.ExpiresIn != 0 {
+		tok.Expiry = time.Now().Add(time.Duration(r.ExpiresIn) * time.Second)
+	}
+	return &tok
 }
